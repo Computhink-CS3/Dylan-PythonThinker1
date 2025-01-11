6f9fae8b41022c8f67934bcf6b7f2a6e2891c853
Computhink-CS3 <cs3@computhink.com.sg>
2025-01-11 07:11:02 +0000

    Jan 11, 2025, 7:11 AM

--- CT06_01/lesson1.py
+++ CT06_01/lesson1.py
@@ -4,7 +4,7 @@
 # print(" i attend anderson primary school")
 # print("if i had $2000 as a reward i would go all in on black")
 print("1. wake up")
-print
+print("2. brush teeeth")
 #print("my favourite food is pizza")
 #print("my birthday is on 28 march")
 ######### The below is the code required for task 2a ##########
