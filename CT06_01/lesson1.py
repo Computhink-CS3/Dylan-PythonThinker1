f136a2f234e964ac1376deb0e1f008727ecc7bb5
Computhink-CS3 <cs3@computhink.com.sg>
2025-01-11 07:03:59 +0000

    Jan 11, 2025, 7:03 AM

--- CT06_01/lesson1.py
+++ CT06_01/lesson1.py
@@ -1,5 +1,6 @@
 #print("Hello world")
 print("my name is dylan")
+print 
 #print("my favourite food is pizza")
 #print("my birthday is on 28 march")
 ######### The below is the code required for task 2a ##########
